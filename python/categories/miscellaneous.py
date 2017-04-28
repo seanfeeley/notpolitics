9b4faca561da006e1e076a51a37c9c8094d12ad6
elliott <elliott@elliotts-MacBook-Pro.local>
2017-04-28 01:56:14 +0100

    Refactoring Misc class.

--- python/categories/miscellaneous.py
+++ python/categories/miscellaneous.py
@@ -5,6 +5,7 @@ locale.setlocale( locale.LC_ALL, '' )
 import re
 from categories import Category
 from items import MiscellaneousItem
+from utils import regex_for_registered, regex_for_amount
 
 class Miscellaneous(Category):
 	def __init__(self):
@@ -21,46 +22,19 @@ class Miscellaneous(Category):
 		self.category_description = 'Miscellaneous'
 		self.isCurrency = True
 
-	def do_logic(self, raw):
+	def do_logic(self, raw_string):
 		"""
-		Method performing the logic of parsing raw data into dictionary
+		Method performing the logic of parsing raw data into item class
 		"""
 
-		template = {
-						'category_type' : self.category_type,
-						'category_id' : self.category_id,
-						'entry_id' : None,
-						'raw' : None,
-						'registered' : None,
-						'amount' : 0,
-						'pretty' : None
-						}
+		next_id = len(self.entries) + 1
+		item_id = '%04d' % next_id
 
-		registered_regex = re.compile(r"\bRegistered\b \d+ [A-Z][a-z]+ \d+")
-		amount_regex = re.compile(r"£\d+")
+		# not much we can really split on
+		pretty = raw_string.split(' (Registered')[0]
+		registered = regex_for_registered(raw_string)
+		amount = regex_for_amount(raw_string)
 
-		if registered_regex.search(raw.encode('utf-8'), re.UNICODE):
-			template['registered'] = registered_regex.search(raw.encode('utf-8'), re.UNICODE).group().split('Registered ')[-1]
-			registered = registered_regex.search(raw.encode('utf-8'), re.UNICODE).group().split('Registered ')[-1]
-		else:
-			registered = ''
+		self.items.append(MiscellaneousItem(item_id, self.category_id, raw_string, pretty, registered, amount))
 
-		if amount_regex.search(raw.replace(',','').encode('utf-8')):
-			template['amount'] = int(amount_regex.search(raw.replace(',', '').encode('utf-8')).group().split('£')[-1])
-			amount = int(amount_regex.search(raw.replace(',', '').encode('utf-8')).group().split('£')[-1])
-		else:
-			amount = 0
-
-		template['raw'] = raw.replace('(Registered %s)' % template['registered'], '')
-
-		next_num = len(self.entries) + 1
-		template['entry_id'] = '%04d' % next_num
-
-		item_id = '%04d' % next_num
-		category_id = self.category_id
-		raw_string = raw
-		pretty = raw.split(' (Regis')[0]
-
-		self.items.append(MiscellaneousItem(item_id, category_id, raw_string, pretty, registered, amount))
-
-		return template
+		return
